39d6c0dc0c7bcf3652d4b53837c0f5f431ca873c
huhuang03 <huhuang03@gmail.com>
2025-11-27 14:05:17 +0800

    feat: show image

--- composeApp/src/jvmMain/kotlin/com/th/pictool/pictool/App.kt
+++ composeApp/src/jvmMain/kotlin/com/th/pictool/pictool/App.kt
@@ -15,7 +15,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
 import pictool.composeapp.generated.resources.Res
 import pictool.composeapp.generated.resources.compose_multiplatform
 
--- composeApp/src/jvmMain/kotlin/com/th/pictool/pictool/main.kt
+++ composeApp/src/jvmMain/kotlin/com/th/pictool/pictool/main.kt
@@ -1,13 +1,68 @@
 package com.th.pictool.pictool
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.toComposeImageBitmap
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyShortcut
+import androidx.compose.ui.window.MenuBar
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import java.awt.FileDialog
+import java.awt.Frame
+import java.awt.image.BufferedImage
+import java.io.File
+import javax.imageio.ImageIO
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "pictool",
     ) {
-        App()
+        var imageBitmap by remember { mutableStateOf<ImageBitmap?>(null) }
+
+        MenuBar {
+            Menu("File") {
+                Item(
+                    "Open",
+                    shortcut = KeyShortcut(Key.O),
+                    onClick = {
+                        val path = chooseImage()
+                        if (path != null) {
+                            // 读取图片
+                            val img = ImageIO.read(File(path))
+                            imageBitmap = img.toImageBitmap()
+                        }
+                    })
+            }
+        }
+        Box() {
+            if (imageBitmap != null) {
+                Image(
+                    bitmap = imageBitmap!!,
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+        }
     }
+}
+
+fun BufferedImage.toImageBitmap(): ImageBitmap = this.toComposeImageBitmap()
+
+// 使用 AWT 原生 FileDialog 选择图片
+fun chooseImage(): String? {
+    val fd = FileDialog(null as Frame?, "Choose an image", FileDialog.LOAD)
+    fd.isVisible = true
+    val file = fd.file
+    val dir = fd.directory
+    return if (file != null && dir != null) "$dir$file" else null
 }
